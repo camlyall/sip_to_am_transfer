5f9f29c5a07306330a43815bb5ea6091d986e218
Cam <cameronlogin@gmail.com>
2022-03-28 10:50:25 +0100

    add logging

--- sip_to_am_transfer.py
+++ sip_to_am_transfer.py
@@ -3,6 +3,7 @@ import json
 import shutil
 import sys
 import xmltodict
+import logging
 from pathlib import Path
 
 
@@ -13,13 +14,13 @@ def validate(sip_dir: Path, output_dir: Path) -> bool:
                 if output_dir.is_dir():
                     return True
                 else:
-                    print("Error: Output is not a directory")
+                    logging.fatal("Error: Output is not a directory")
             else:
                 return True
         else:
-            print("Error: Input is not a directory")
+            logging.fatal("Error: Input is not a directory")
     else:
-        print("Error: Input directory doesn't exit")
+        logging.fatal("Error: Input directory doesn't exit")
     return False
 
 
@@ -28,7 +29,8 @@ def dc_xml_to_json(dc_file):
         try:
             obj = xmltodict.parse(xml.read())['simpledc']
         except KeyError:
-            print('Incorrect metadata format. Expected simpledc.')
+            logging.error('Incorrect metadata format. Expected simpledc.')
+            # print('Incorrect metadata format. Expected simpledc.')
         else:
             metadata_dict = {"filename": "object/representations"}
             for key in obj:
@@ -47,27 +49,35 @@ def transform(sip_dir: Path, output_dir: Path):
     dc_metadata_file = sip_dir / "metadata" / "descriptive" / "dc.xml"
     dc_json_data = ''
     if dc_metadata_file.is_file():
+        logging.info('DC.xml metadata found')
         dc_json_data = dc_xml_to_json(dc_metadata_file)
+        logging.info(dc_json_data)
+    else:
+        logging.info('DC.xml metadata not found')
 
     for sip_rep_dir in sip_representations_dir.iterdir():
         if sip_rep_dir.is_dir():
             transfer_name = sip_name + '-' + sip_rep_dir.name
             transfer_output_dir = output_dir / transfer_name
             if transfer_output_dir.is_dir():
-                print(f"Overwriting {transfer_output_dir}")
+                logging.info(f"Overwriting {transfer_output_dir}")
                 shutil.rmtree(transfer_output_dir)
             transfer_output_dir.mkdir(parents=True, exist_ok=False)
 
             # Copy representations folder(s)
             shutil.copytree(sip_rep_dir, transfer_output_dir / "representation")
+            logging.info('Representations copied')
 
             # Write DC metadata json
             if dc_json_data != '':
                 (transfer_output_dir / "metadata").mkdir(parents=False, exist_ok=False)
                 with open(transfer_output_dir / "metadata" / "metadata.json", "w") as json_file:
                     json_file.write("["+dc_json_data+"]")
+                    logging.info('metadata.json written')
         else:
-            print('Warning: File found in representations')
+            logging.fatal('Warning: File found in representations')
+            sys.exit(2)
+    logging.info('COMPLETE!')
 
 
 def main(argv):
@@ -76,35 +86,41 @@ def main(argv):
     try:
         opts, _ = getopt.getopt(argv,"hi:o:",["input=, output="])
     except getopt.GetoptError:
-        print('metsgen.py -i <SIP directory> -o <Output Directory>')
+        logging.fatal('Incorrect script call format.')
+        print("Error: Command should have the form:")
+        print('python sip_to_am_transfer.py -i <SIP directory> -o <Output Directory>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('metsgen.py -i <SIP Directory> -o <Output Directory>')
+            print('python sip_to_am_transfer.py -i <SIP Directory> -o <Output Directory>')
             sys.exit(2)
         elif opt in ("-i", "--input"):
             input_arg = Path(arg)
             if input_arg.is_dir() and (input_arg / 'representations').is_dir():
                 sip_dir = input_arg
             else:
+                logging.fatal('Invalid SIP')
                 print("Input is not a valid SIP directory")
                 sys.exit(2)
         elif opt in ("-o", "--output"):
             output_arg = Path(arg)
             if not output_arg.is_dir():
-                print("Creating output directoy:", output_arg)
+                logging.info('Creating output directory:', output_arg.stem)
+                # print("Creating output directoy:", output_arg)
                 output_arg.mkdir(parents=True, exist_ok=False)
             output_dir = output_arg
     if sip_dir == '':
-        print("No SIP given")
+        logging.fatal("No SIP given")
         sys.exit(2)
     if output_dir == '':
-        print("No Output directory given")
+        logging.fatal("No output directory given")
         sys.exit(2)
     if validate(sip_dir, output_dir):
         transform(sip_dir, output_dir)
 
 
 if __name__ == "__main__":
+    Path("logs").mkdir(exist_ok=True)
+    logging.basicConfig(level=logging.DEBUG, filemode='a', filename='logs/sip_to_am_transfer.log', format='%(asctime)s %(levelname)s: %(message)s')
     main(sys.argv[1:])
     
